fac694fcb6be70c8374f74caa710df3f56d2ba79
leejohy-0223 <leejohy@naver.com>
2022-07-14 20:26:12 +0900

    codeit 100 풀이 완료

--- python/test.py
+++ python/test.py
@@ -1,1 +1,24 @@
-print('Hello World!')
+arr = [list(map(int, input().split())) for _ in range(10)]
+
+x, y = 1, 1
+
+while True:
+
+    if arr[x][y] == 1:
+        break
+
+    if arr[x][y] == 0:
+        arr[x][y] = 9
+        if arr[x][y + 1] == 1:
+            x += 1
+        else:
+            y += 1
+    
+    if arr[x][y] == 2:
+        arr[x][y] = 9
+        break
+
+for i in arr:
+    for j in i:
+        print(j, end=" ")
+    print()
